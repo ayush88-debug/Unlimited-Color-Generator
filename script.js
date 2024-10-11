84fd7471a1d99f50102d25201a99783daad5c892
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2024-10-11 16:34:40 +0530

    updated

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let stop=document.querySelector("#stop")
 let body=document.querySelector("body")
 
 let IntervalID
+const letters="0123456789ABCDEF"
+
 
 start.addEventListener('click', function(e){
     body.setAttribute('autofocus','')
@@ -10,7 +12,12 @@ start.addEventListener('click', function(e){
     document.querySelector(".message").innerHTML=`Press any key to stop`
     if(!IntervalID){
         IntervalID=setInterval(() => {
-            let randomColor= Math.floor(Math.random()*(999999-100000+1))+100000
+            let randomColor=""
+            for(let i=0;i<6;i++){
+                let letter=Math.floor(Math.random()*16)
+                randomColor+=letters[letter]
+            }
+
             console.log(randomColor)
             body.style.backgroundColor=`#${randomColor}`
             document.querySelector(".result").textContent=`#${randomColor}`
